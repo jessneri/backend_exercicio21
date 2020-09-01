41506cdf6fc1e3d101787e76d2e6c37f50800f6e
Jéssica Neri <jess.neri94@gmail.com>
2020-09-01 20:38:10 -0300

    Doc: novo servidor web utilizando Koa

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Koa = require("koa"); // faz importação do código koa
 const server = new Koa(); //instanciando um novo objeto koa
 
 server.use(async(ctx) => {
-    console.log("Hey! Esta é a aula 21!");
+    ctx.body = "Hey! Esta é a aula 21!";
 });
 
 server.listen(8081, () => {
